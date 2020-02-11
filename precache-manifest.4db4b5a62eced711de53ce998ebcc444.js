cdfaa65ade21d12f5f39ecdbdc28b10e631ea948
Halil Ibrahim Kayim <halil.kayim@protonmail.com>
2020-02-12 00:16:56 +0300

    Updates

---
+++ precache-manifest.4db4b5a62eced711de53ce998ebcc444.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "549cd671af1c1bd1c24ec2241a099a20",
+    "url": "/release-diff/index.html"
+  },
+  {
+    "revision": "915835fbfd3eadb928bd",
+    "url": "/release-diff/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b8355263ece25bad67ea",
+    "url": "/release-diff/static/js/2.e4b207ef.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/release-diff/static/js/2.e4b207ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "915835fbfd3eadb928bd",
+    "url": "/release-diff/static/js/main.9054b22a.chunk.js"
+  },
+  {
+    "revision": "daed0b98357127eb9f3f",
+    "url": "/release-diff/static/js/runtime-main.5a55b15e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/release-diff/static/media/logo.5d5d9eef.svg"
+  }
+]);
